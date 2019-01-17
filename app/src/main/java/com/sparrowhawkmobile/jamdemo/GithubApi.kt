3c50adb447ef8597ceee752296363d6370cfa98b
Marek Krogulski <marek.krogulski@appdate.pl>
2019-01-17 19:43:21 +0100

    Cleanup on layouts Added needed libraries Added API base

---
+++ app/src/main/java/com/sparrowhawkmobile/jamdemo/GithubApi.kt
@@ -0,0 +1,14 @@
+package com.sparrowhawkmobile.jamdemo
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface GithubApi {
+
+    @GET("search/repositories")
+    fun getRepositoriesByQuery(
+        @Query("q") queryText: String
+    ): Single<Any>
+}
